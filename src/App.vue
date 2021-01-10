e0bbd06921d33c8816a87d0485d6033c2d7a291b
DevDarl <darlingtonokoriec@gmail.com>
2021-01-10 21:26:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  <products-list-one></products-list-one>
+  <products-list-two></products-list-two>  
+  </div>
+</template>
+
+<script>
+import ProductsListOne from "./components/ProductsListOne";
+import ProductsListTwo from "./components/ProductsListTwo";
+
+export default {
+  name: 'App',
+  components:{
+    'products-list-one': ProductsListOne,
+    'products-list-two': ProductsListTwo
+  },
+  data(){
+    return{
+      
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>
